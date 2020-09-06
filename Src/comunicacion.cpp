4259084d48e5178635e503b4e440396ecc13bd68
Samir Gattás <samirgattas@gmail.com>
2020-09-06 10:08:47 -0300

    Genera el servidor y recibe un 1 o 0 desde el cliente. Apaga y enciende el led en funcion dedl valor recibido

--- Src/comunicacion.cpp
+++ Src/comunicacion.cpp
@@ -5,27 +5,50 @@
  *      Author: xx
  */
 
+// ############################################################################
+// INCLUDE
 #include "comunicacion.h"
+
+
+// ############################################################################
+// DEFINE
+#ifdef SERVER
+#define MAC				0xE4, 0xAB, 0x89, 0x1B, 0xF1, 0xA1
+#define IP_PROPIA 		192, 168, 88, 30
+#define IP_SERVIDOR		192, 168, 88, 70
+#elif CLIENT
+#define MAC				0xE4, 0xAB, 0x89, 0x1B, 0xF1, 0xA0
+#define IP_PROPIA 		192, 168, 88, 40
+#define IP_SERVIDOR		192, 168, 88, 30
+#endif
+#define PUERTO			3030
+
+// ############################################################################
+// VARIABLES INTERNAS
 
 extern SPI_HandleTypeDef hspi1;
 extern UART_HandleTypeDef huart1;
 
 //uint8_t mac[6] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-uint8_t mac[6] = { 0xE4, 0xAB, 0x89, 0x1B, 0xF1, 0xA0};
+uint8_t mac[6] = {MAC};
 //char server[] = "www.google.com";
-IPAddress ip_server(172, 217, 162, 4);
+IPAddress ip_server(IP_SERVIDOR);
 
-IPAddress ip(192, 168, 1, 70);
+IPAddress ip(IP_PROPIA);
 IPAddress myDns(192, 168, 0, 1);
 EthernetClient client;
-
-#ifdef SERVER
-EthernetServer server(80);
-#endif
+EthernetServer server(PUERTO);
 
 int8_t byteCount = -1;
 
 static uint8_t buffer[1024];
+
+// ############################################################################
+// FUNCIONES INTERNAS
+
+
+// ############################################################################
+// FUNCIONES EXTERNAS
 
 // ---------- Cliente -------------
 
@@ -50,7 +73,7 @@ void connect_to_server() {
 	HAL_Delay(1000);
 	HAL_UART_Transmit(&huart1, (uint8_t *)"Conectando\n", 11, 100);
 	// if you get a connection, report back via serial:
-	if (client.connect(ip_server, 80)) {
+	if (client.connect(ip_server, PUERTO)) {
 		HAL_UART_Transmit(&huart1, (uint8_t *)"Conectado\n", 10, 100);
 		// Make a HTTP request:
 		//client.println("GET /search?q=arduino HTTP/1.1"); // for Google
@@ -114,15 +137,22 @@ void escuchar_cliente() {
 	char c;
 	bool currentLineIsBlank;;
 	uint8_t msj;
+
+	uint8_t tout = 10;
+
 	// Crea conexion cliente
 	client = server.available();
 	if (!client) return;
 	currentLineIsBlank = true;
 	while (client.connected()) {
 		if (client.available()) {
+			/*if (tout == 0) {
+				break;
+			}*/
 			c = client.read();
 			msj = (uint8_t) c;
 			HAL_UART_Transmit(&huart1, &msj, 1, 100);
+			/*
 			if (c == '\n' && currentLineIsBlank) {
 				client.println("HTTP/1.1 200 OK"); //envia una nueva pagina en codigo HTML
 				client.println("Content-Type: text/html");
@@ -136,6 +166,14 @@ void escuchar_cliente() {
 				client.println("<BODY>");
 				break;
 			}
+			*/
+			if (c == '1') {
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+				break;
+			} else {
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+				break;
+			}
 			if (c == '\n') {
 				// you're starting a new line
 				currentLineIsBlank = true;
@@ -143,6 +181,7 @@ void escuchar_cliente() {
 				// you've gotten a character on the current line
 				currentLineIsBlank = false;
 			}
+			tout--;
 		}
 	}
 	HAL_UART_Transmit(&huart1, (uint8_t *)"\nFin\n", 4, 100);
@@ -150,3 +189,4 @@ void escuchar_cliente() {
 	client.stop();
 }
 
+// ############################################################################
